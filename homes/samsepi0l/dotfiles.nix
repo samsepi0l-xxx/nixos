588a889de1e8dd07b5c4f502d7466cfefb4a1dc4
Sam Sepiol <samsepi0I@tutamail.com>
2025-01-26 01:07:58 -0300

    add nyoom neovim config

--- homes/samsepi0l/dotfiles.nix
+++ homes/samsepi0l/dotfiles.nix
@@ -1,5 +1,6 @@
 { config, impurity, inputs, pkgs, ... }: {
   xdg.configFile = let link = impurity.link; in {
+    "nvim".source = link ./.config/nvim;
     # "ags".source = link ./.
     # "thorium-flags.conf".source = link ./.config/thorium-flags.co
     # "starship.toml".source = link ./.config/starship.toml;
