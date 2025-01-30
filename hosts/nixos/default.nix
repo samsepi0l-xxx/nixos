2904af6a2446c71a00b25681aabe0513eeefdfda
samsepi0l <samsepi0I@tutamail.com>
2025-01-30 15:32:31 -0300

    activing services

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -3,45 +3,44 @@
     # ./amd.nix
     ./audio.nix
     # ./auto-upgrade.nix
-    #./bluetooth.nix
+    ./bluetooth.nix
     ./bootloader.nix
-    # ./clamav-scanner.nix
+    ./clamav-scanner.nix
     ./configuration.nix
     ./display-manager.nix
-    #./dns.nix
-    #./environment-variables.nix
-    #./firewall.nix
-    #./fonts.nix
-    #./gc.nix
+    ./dns.nix
+    ./environment-variables.nix
+    ./firewall.nix
+    ./fonts.nix
+    ./gc.nix
     ./hardware-configuration.nix
-    #./hyprland.nix
-    #./info-fetchers.nix
+    ./info-fetchers.nix
     ./keyboard.nix
     ./kernel.nix
-    #./llm.nix
+    ./llm.nix
     ./locale.nix
-    #./lsp.nix
+    ./lsp.nix
     # ./mac-randomize.nix
     ./networking.nix
     ./nix-settings.nix
     ./nixpkgs.nix
     # ./open-ssh.nix
-    #./opengl.nix
-    #./programming-languages.nix
-    #./security-services.nix
-    #./security-tools.nix
+    ./opengl.nix
+    ./programming-languages.nix
+    ./security-services.nix
+    ./security-tools.nix
     ./services.nix
     ./swap.nix
-    #./terminal-utils.nix
-    #./theme.nix
-    #./usb.nix
+    ./terminal-utils.nix
+    ./theme.nix
+    ./usb.nix
     ./users.nix
-    #./utils.nix
-    #./virtualisation.nix
-    #./vpn.nix
+    ./utils.nix
+    ./virtualisation.nix
+    ./vpn.nix
     # ./wasm.nix
-    #./work.nix
-    #./yubikey.nix
+    ./work.nix
+    ./yubikey.nix
   ];
 }
 
