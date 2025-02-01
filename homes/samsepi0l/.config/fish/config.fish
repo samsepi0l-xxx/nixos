7f2d704eee3633078d6429674f0468740ac55981
samsepi0l <samsepi0I@tutamail.com>
2025-01-31 23:47:30 -0300

    add ssh key to fish

--- homes/samsepi0l/.config/fish/config.fish
+++ homes/samsepi0l/.config/fish/config.fish
@@ -6,3 +6,11 @@ function fish_prompt
     set_color green 
 end
 
+## SSH
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+end
+
+if not ssh-add -l | grep -q "$(cat ~/.ssh/github_samsepi0l.pub | awk '{print $2}')"
+    ssh-add ~/.ssh/github_samsepi0l &> /dev/null
+end
