d10dd00d8bd67829239e157dc2a964e83c7a57ed
samsepi0l <samsepi0I@tutamail.com>
2025-02-02 13:08:26 -0300

    remove fish link symbol

--- homes/samsepi0l/dotfiles.nix
+++ homes/samsepi0l/dotfiles.nix
@@ -1,7 +1,7 @@
 { config, impurity, inputs, pkgs, ... }: {
   xdg.configFile = let link = impurity.link; in {
     "nvim".source = link ./.config/nvim;
-    "fish".source = link ./.config/fish;
+    #"fish".source = link ./.config/fish;
     # "ags".source = link ./.
     # "thorium-flags.conf".source = link ./.config/thorium-flags.co
     # "starship.toml".source = link ./.config/starship.toml;
--- hosts/nixos/environment-variables.nix
+++ hosts/nixos/environment-variables.nix
@@ -6,5 +6,4 @@
   environment.variables.JDK_PATH = "${pkgs.jdk17}/";
   environment.variables.NODEJS_PATH = "${pkgs.nodePackages_latest.nodejs}/";
   environment.variables.IMPURITY_PATH = "~/GitHub/nixos";
-  environment.variables.XDG_CONFIG_HOME = "~/.config";
 }
